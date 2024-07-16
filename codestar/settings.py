5d0f1fe9a89a20d14a3e129297c5d732f937de75
Riley North <rileycampling@gmail.com>
2024-07-16 21:53:02 +0000

    slight tweeks in requirments

--- codestar/settings.py
+++ codestar/settings.py
@@ -84,12 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        }
+       }
 }
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
