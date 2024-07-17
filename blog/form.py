03b83a8ecbfe1793a563317f0e860a37ad6612a4
Riley North <rileycampling@gmail.com>
2024-07-17 23:38:38 +0000

    added form validation and double booking features

---
+++ blog/form.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Booking
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['date', 'booked']
+
+    def clean_date(self):
+        date = self.cleaned_data.get('date')
+        if Booking.objects.filter(date=date, booked=True).exists():
+            raise forms.ValidationError("This date is already booked.")
+        return date
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from .forms import BookingForm
+
+@login_required
+def index(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.save()
+            return redirect('index')
+    else:
+        form = BookingForm()
+
+    bookings = Booking.objects.all().order_by('date')
+    context = {
+        'bookings': bookings,
+        'form': form,
+    }
+    return render(request, 'blog/index.html', context)
+
+
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -22,9 +44,3 @@ def post_detail(request, slug):
     )
 
     
-def index(request):
-    bookings = Booking.objects.all().order_by('date')
-    context = {
-        'bookings': bookings,
-    }
-    return render(request, 'blog/index.html', context)
